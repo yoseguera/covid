f590a0b9c0bc62da9487cc5e146b6ff28ad1407d
M. Yago Oseguera Saura <yago.oseguera@outlook.com>
2021-01-24 14:00:12 +0100

    Limpieza

--- covid.R
+++ covid.R
@@ -1,142 +1,73 @@
-datos <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")
-paises <- c("Spain", "Germany", "France", "Belgium", "United_Kingdom", "Italy", "Netherlands", "Singapore", "Japan", "South_Korea", "United_States_of_America")
+library(ggplot2)
+library(scales)
 
-maxX <- 0
-maxY <- 0
-for (i in 1:length(paises))
-{    
-    pais <- paises[i]
-    condicion <- datos[,"countriesAndTerritories"] == pais
-    datos_pais <- rev(datos[condicion, "deaths"]/datos[condicion, "popData2018"]*1000000)
-    datos_pais <- cumsum(datos_pais)
-    datos_pais <- datos_pais[datos_pais>1]
-    if (maxX < length(datos_pais))
+calcular_derivada <- function(datos) {
+    diferencia <- rep(0, nrow(datos))    
+    
+    for (i in 2:(nrow(datos)))
     {
-        maxX <- length(datos_pais)
-    }        
-    if (maxY < max(datos_pais)) 
-    {
-        maxY <- max(datos_pais)  
+        diferencia[i] <- (datos$ia14d[i] - datos$ia14d[i-1])/(as.numeric(datos$fecha[i]) - as.numeric(datos$fecha[i-1]))
     }
+    
+    result <- diferencia
 }
 
-png(file = "muertes-mundo.png")
-plot(0, type="l", xlim=c(0,maxX), ylim=c(1,maxY))
-for (i in 1:length(paises))
-{    
-    pais <- paises[i]
-    condicion <- datos[,"countriesAndTerritories"] == pais
-    datos_pais <- rev(datos[condicion, "deaths"]/datos[condicion, "popData2018"]*1000000)
-    datos_pais <- cumsum(datos_pais)
-    datos_pais <- datos_pais[datos_pais>1]
-    lines(datos_pais, col=i, lty=i, type="l")     
-}
-legend("bottomleft", legend=paises, col=1:length(paises), lty=1:length(paises))
+datos <- read.csv("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_ia14d.csv")
+datos_calculados <- subset(datos, cod_ine == 8)
+datos_calculados$fecha <- as.Date(datos_calculados$fecha)
+datos_calculados$DIF_CLM = calcular_derivada(datos_calculados)
+
+datos_sim <- datos[c(1,4)]
+datos_es <- aggregate(datos_sim$ia14d, by = list(datos_sim$fecha), FUN = sum, drop=TRUE)
+datos_es$fecha <- datos_es$Group.1
+datos_es$ia14d <- datos_es$x/17
+datos_calculados$fecha <- as.Date(datos_calculados$fecha)
+datos_calculados$IA14_ES = datos_es$ia14d
+datos_calculados$DIF_ES = calcular_derivada(datos_es)
+
+
+
+leyenda <- c("Castilla-La Mancha", "España")
+
+png(file = "ia14.png")
+ggplot(data=datos_calculados, aes(x=fecha)) +
+    geom_line(aes(y = ia14d, group = 1), color = "red") +
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = ia14d, group = 1), color = "red") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = ia14d, label = sprintf("%0.2f", round(ia14d, digits = 2))), position = position_dodge(width = 1), color = "red") +
+    geom_line(aes(y = IA14_ES, group = 2), color = "blue") + 
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = IA14_ES, group = 1), color = "blue") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = IA14_ES, label = sprintf("%0.2f", round(IA14_ES, digits = 2))), color = "red") 
+    #geom_line(aes(y = IA7_ES, group = 3), color = "black") +
+    #geom_point(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, group = 1), color = "black") +
+    #geom_text(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, label = sprintf("%0.2f", round(IA7_ES, digits = 2))), color = "red")    
+    
 dev.off()
 
-maxX <- 0
-maxY <- 0
-for (i in 1:length(paises))
-{    
-    pais <- paises[i]
-    condicion <- datos[,"countriesAndTerritories"] == pais
-    datos_pais <- rev(datos[condicion, "cases"]/datos[condicion, "popData2018"]*1000000)
-    datos_pais <- cumsum(datos_pais)
-    datos_pais <- datos_pais[datos_pais>1]
-    if (maxX < length(datos_pais)) 
-    {
-        maxX <- length(datos_pais)
-    }
-    if (maxY < max(datos_pais))
-    {
-        maxY <- max(datos_pais)  
-    }
-}
-
-png(file = "casos-mundo.png")
-plot(0, type="l", xlim=c(0,maxX), ylim=c(1,maxY))
-for (i in 1:length(paises))
-{    
-    pais <- paises[i]
-    condicion <- datos[,"countriesAndTerritories"] == pais
-    datos_pais <- rev(datos[condicion, "cases"]/datos[condicion, "popData2018"]*1000000)
-    datos_pais <- cumsum(datos_pais)
-    datos_pais <- datos_pais[datos_pais>1]
-    lines(datos_pais, col=i, lty=i, type="l")     
-}
-legend("bottomleft", legend=paises, col=1:length(paises), lty=1:length(paises))
+png(file = "ia14-log.png")
+ggplot(data=datos_calculados, aes(x=fecha)) +
+    geom_line(aes(y = ia14d, group = 1), color = "red") +
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = ia14d, group = 1), color = "red") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = ia14d, label = sprintf("%0.2f", round(ia14d, digits = 2))), position = position_dodge(width = 1), color = "red") +
+    geom_line(aes(y = IA14_ES, group = 2), color = "blue") + 
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = IA14_ES, group = 1), color = "blue") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = IA14_ES, label = sprintf("%0.2f", round(IA14_ES, digits = 2))), color = "red") +
+    scale_y_continuous(trans='log10')
+    #geom_line(aes(y = IA7_ES, group = 3), color = "black") +
+    #geom_point(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, group = 1), color = "black") +
+    #geom_text(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, label = sprintf("%0.2f", round(IA7_ES, digits = 2))), color = "red")    
+    
 dev.off()
 
-datos <- read.csv("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_casos_long.csv")
-ccaas <- read.csv("ccaa.csv", stringsAsFactors=FALSE)
-maxX <- 0
-maxY <- 0
-for (i in 1:nrow(ccaas))
-{    
-    ccaa <- ccaas[i,"nombre"]
-    poblacion <- ccaas[i,"poblacion"]
-    condicion <- datos[,"CCAA"] == ccaa
-    datos_ccaa <- rev(datos[condicion, "total"]/poblacion*1000000)    
-    datos_ccaa <- cumsum(datos_ccaa)
-    datos_ccaa <- datos_ccaa[datos_ccaa>1]
-    if (maxX < length(datos_ccaa))
-    {
-        maxX <- length(datos_ccaa)
-    }
-    if (maxY < max(datos_ccaa))
-    {
-        maxY <- max(datos_ccaa)  
-    }
-}
-
-png(file = "casos-ccaa.png")
-plot(0, type="l", xlim=c(0,maxX), ylim=c(500,maxY))
-for (i in 1:nrow(ccaas))
-{    
-    ccaa <- ccaas[i,"nombre"]
-    poblacion <- ccaas[i,"poblacion"]
-    condicion <- datos[,"CCAA"] == ccaa
-    datos_ccaa <- rev(datos[condicion, "total"]/poblacion*1000000)    
-    datos_ccaa <- cumsum(datos_ccaa)
-    datos_ccaa <- datos_ccaa[datos_ccaa>1]
-    lines(datos_ccaa, col=i, lty=i, type="l")     
-}
-legend("bottomleft", legend=ccaas[,"nombre"], col=1:nrow(ccaas), lty=1:nrow(ccaas))
-dev.off()
-
-datos <- read.csv("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv")
-ccaas <- read.csv("ccaa.csv", stringsAsFactors=FALSE)
-maxX <- 0
-maxY <- 0
-for (i in 1:nrow(ccaas))
-{    
-    ccaa <- ccaas[i,"nombre"]
-    poblacion <- ccaas[i,"poblacion"]
-    condicion <- datos[,"CCAA"] == ccaa
-    datos_ccaa <- rev(datos[condicion, "total"]/poblacion*1000000)    
-    datos_ccaa <- cumsum(datos_ccaa)
-    datos_ccaa <- datos_ccaa[datos_ccaa>1]
-    if (maxX < length(datos_ccaa))
-    {
-        maxX <- length(datos_ccaa)
-    }
-    if (maxY < max(datos_ccaa))
-    {
-        maxY <- max(datos_ccaa) 
-    }
-}
-
-png(file = "muertos-ccaa.png")
-plot(0, type="l", xlim=c(0,maxX), ylim=c(100,maxY))
-for (i in 1:nrow(ccaas))
-{    
-    ccaa <- ccaas[i,"nombre"]
-    poblacion <- ccaas[i,"poblacion"]
-    condicion <- datos[,"CCAA"] == ccaa
-    datos_ccaa <- rev(datos[condicion, "total"]/poblacion*1000000)    
-    datos_ccaa <- cumsum(datos_ccaa)
-    datos_ccaa <- datos_ccaa[datos_ccaa>1]
-    lines(datos_ccaa, col=i, lty=i, type="l")     
-}
-legend("bottomleft", legend=ccaas[,"nombre"], col=1:nrow(ccaas), lty=1:nrow(ccaas))
+png(file = "derivada.png")
+ggplot(data=datos_calculados, aes(x=fecha)) +
+    geom_line(aes(y = DIF_CLM, group = 1), color = "red") +
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = DIF_CLM, group = 1), color = "red") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = DIF_CLM, label = sprintf("%0.2f", round(DIF_CLM, digits = 2))), position = position_dodge(width = 1), color = "red") + 
+    geom_line(aes(y = DIF_ES, group = 2), color = "blue") + 
+    geom_point(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = DIF_ES, group = 1), color = "blue") +
+    geom_text(data = datos_calculados[c(nrow(datos_calculados)),], aes(y = DIF_ES, label = sprintf("%0.2f", round(DIF_ES, digits = 2))), color = "red") #+
+    #geom_line(aes(y = IA7_ES, group = 3), color = "black") +
+    #geom_point(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, group = 1), color = "black") +
+    #geom_text(data = datos_ab[c(nrow(datos_ab)),], aes(y = IA7_ES, label = sprintf("%0.2f", round(IA7_ES, digits = 2))), color = "red")    
+    
 dev.off()
